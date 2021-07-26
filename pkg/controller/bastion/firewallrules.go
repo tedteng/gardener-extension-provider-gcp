fbad5cfe22865b73a53ff0684b28bd8563af8e5d
Ted Teng <ted.teng@sap.com>
2021-07-26 15:55:39 +0800

    introduce firewall rules allow/denyall,denyallowonly

---
+++ pkg/controller/bastion/firewallrules.go
@@ -0,0 +1,74 @@
+// Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bastion
+
+import (
+	"fmt"
+	"strconv"
+
+	"google.golang.org/api/compute/v1"
+)
+
+type rule int
+
+const (
+	ingressAllowSSH rule = iota + 1
+	egressDenyAll
+	egressAllowOnly
+)
+
+func firewallRule(opt *Options, name rule) *compute.Firewall {
+	//https://cloud.google.com/vpc/docs/using-firewalls
+	switch name {
+	case ingressAllowSSH:
+		return &compute.Firewall{
+			Allowed:      []*compute.FirewallAllowed{{IPProtocol: "tcp", Ports: []string{strconv.Itoa(SSHPort)}}},
+			Description:  "SSH access for Bastion",
+			Direction:    "INGRESS",
+			TargetTags:   []string{opt.BastionInstanceName},
+			Name:         opt.BastionInstanceName + "-allow-ssh",
+			Network:      opt.Network,
+			SourceRanges: opt.CIDRs,
+			Priority:     50,
+		}
+
+	case egressDenyAll:
+		return &compute.Firewall{
+			Denied:            []*compute.FirewallDenied{{IPProtocol: "all"}},
+			Description:       "SSH access for Bastion engress deny",
+			Direction:         "EGRESS",
+			TargetTags:        []string{opt.BastionInstanceName},
+			Name:              opt.BastionInstanceName + "-denyall",
+			Network:           opt.Network,
+			DestinationRanges: []string{"0.0.0.0/0"},
+			Priority:          1000,
+		}
+
+	case egressAllowOnly:
+		return &compute.Firewall{
+			Allowed:           []*compute.FirewallAllowed{{IPProtocol: "all"}},
+			Description:       "SSH access for Bastion engress deny",
+			Direction:         "EGRESS",
+			TargetTags:        []string{opt.BastionInstanceName},
+			Name:              opt.BastionInstanceName + "-egressallowonly",
+			Network:           opt.Network,
+			DestinationRanges: []string{"10.250.0.0/16"},
+			Priority:          60,
+		}
+	default:
+		fmt.Println("Rule not config in firewallrules.go")
+	}
+	return nil
+}
