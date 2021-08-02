3563590dc44e7dc539f78992ad611162d8d84151
Andrei Panov <andrei.panov@sap.com>
2021-08-02 13:20:05 +0200

    migrate tests to ginkgo

--- pkg/controller/bastion/bastion_suite_test.go
+++ pkg/controller/bastion/bastion_suite_test.go
@@ -1,6 +1,8 @@
-package bastion_test
+package bastion
 
 import (
+	"reflect"
+	"runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -11,3 +13,47 @@ func TestBastion(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Bastion Suite")
 }
+
+var _ = Describe("Bastion", func() {
+	Describe("check Names generations", func() {
+		It("should generate idempotent name", func() {
+			expected := "clusterName-shortName-bastion-79641"
+
+			res, err := generateBastionBaseResourceName("clusterName", "shortName")
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(res).To(Equal(expected))
+
+			res, err = generateBastionBaseResourceName("clusterName", "shortName")
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(res).To(Equal(expected))
+		})
+
+		It("should generate a name not exceeding a certain length", func() {
+			res, err := generateBastionBaseResourceName("clusterName", "LetsExceed63LenLimit012345678901234567890123456789012345678901234567890123456789")
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(res).To(Equal("clusterName-LetsExceed63LenLimit0-bastion-139c4"))
+		})
+
+		It("should generate a unique name even if inputs values have minor deviations", func() {
+			res, _ := generateBastionBaseResourceName("1", "1")
+			res2, _ := generateBastionBaseResourceName("1", "2")
+			Expect(res).ToNot(Equal(res2))
+		})
+
+		It("should generate names and fit maximum length", func() {
+			nameGenerators := []func(string) string{
+				diskResourceName,
+				nodesResourceName,
+				firewallIngressAllowSSHResourceName,
+				firewallEgressAllowOnlyResourceName,
+				firewallEgressDenyAllResourceName,
+			}
+
+			baseName, _ := generateBastionBaseResourceName("clusterName", "LetsExceed63LenLimit012345678901234567890123456789012345678901234567890123456789")
+			for _, fun := range nameGenerators {
+				result := fun(baseName)
+				Expect(len(result)).Should(BeNumerically("<", maxLengthForResource), "failed function: %v", runtime.FuncForPC(reflect.ValueOf(fun).Pointer()).Name())
+			}
+		})
+	})
+})
