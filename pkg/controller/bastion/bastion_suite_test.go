514364ea165816f91ac8925beb5391f99afe0a30
Kenneth Wong <kenneth.wong@sap.com>
2021-08-03 16:40:17 -0700

    Adding more unit tests for options.go

--- pkg/controller/bastion/bastion_suite_test.go
+++ pkg/controller/bastion/bastion_suite_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gardener/gardener/extensions/pkg/controller"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"github.com/gardener/gardener/pkg/extensions"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime2 "k8s.io/apimachinery/pkg/runtime"
@@ -23,6 +24,14 @@ func TestBastion(t *testing.T) {
 }
 
 var _ = Describe("Bastion", func() {
+	var (
+		cluster *extensions.Cluster
+		bastion *extensionsv1alpha1.Bastion
+	)
+	BeforeEach(func() {
+		cluster = createGCPTestCluster()
+		bastion = createTestBastion()
+	})
 
 	Describe("getWorkersCIDR", func() {
 		It("getWorkersCIDR", func() {
@@ -34,44 +43,13 @@ var _ = Describe("Bastion", func() {
 
 	Describe("Determine options", func() {
 		It("should return options", func() {
-
-			cluster := &controller.Cluster{
-				ObjectMeta: metav1.ObjectMeta{Name: "cluster1"},
-				Shoot:      createShootTestStruct(),
-				CloudProfile: &gardencorev1beta1.CloudProfile{
-					Spec: gardencorev1beta1.CloudProfileSpec{
-						Regions: []gardencorev1beta1.Region{
-							{Name: "regionName"},
-							{Name: "us-west", Zones: []gardencorev1beta1.AvailabilityZone{
-								{Name: "us-west-a"},
-								{Name: "us-west-b"},
-							}},
-						},
-					},
-				},
-			}
-
-			bastion := &extensionsv1alpha1.Bastion{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "bastionName1",
-				},
-				Spec: extensionsv1alpha1.BastionSpec{
-					DefaultSpec: extensionsv1alpha1.DefaultSpec{},
-					UserData:    nil,
-					Ingress: []extensionsv1alpha1.BastionIngressPolicy{
-						{IPBlock: networkingv1.IPBlock{
-							CIDR: "213.69.151.0/24",
-						}},
-					},
-				},
-			}
 			options, err := DetermineOptions(bastion, cluster, "projectID")
 			if err != nil {
 				fmt.Println(err)
 			}
 
 			Expect(options.BastionInstanceName).To(Equal("cluster1-bastionName1-bastion-1cdc8"))
-			Expect(options.Zone).To(Equal("us-west-a"))
+			Expect(options.Zone).To(Equal("us-west1-a"))
 			Expect(options.DiskName).To(Equal("cluster1-bastionName1-bastion-1cdc8-disk"))
 			Expect(options.CIDRs).To(Equal([]string{"213.69.151.0/24"}))
 			Expect(options.Subnetwork).To(Equal("regions/us-west/subnetworks/cluster1-nodes"))
@@ -123,6 +101,21 @@ var _ = Describe("Bastion", func() {
 		})
 	})
 
+	Describe("check getZone", func() {
+		var testProviderStatusRaw providerStatusRaw
+		It("should return an empty string", func() {
+			testProviderStatusRaw = providerStatusRaw{""}
+			res := getZone(cluster, "us-west", &testProviderStatusRaw)
+			Expect(res).To(BeEmpty())
+		})
+
+		It("should return a zone string", func() {
+			testProviderStatusRaw = providerStatusRaw{"us-west1-a"}
+			res := getZone(cluster, "us-west", &testProviderStatusRaw)
+			Expect(res).To(Equal("us-west1-a"))
+		})
+	})
+
 	Describe("check marshalProviderStatus", func() {
 		It("should return a JSON object containing a Zone Struct", func() {
 			res, err := marshalProviderStatus("us-west1-a")
@@ -131,8 +124,8 @@ var _ = Describe("Bastion", func() {
 			Expect(err).To(Not(HaveOccurred()))
 			Expect(string(res)).To(Equal(expectedMarshalOutput))
 		})
-
 	})
+
 	Describe("check unMarshalProviderStatus", func() {
 		It("should update a ProviderStatusRaw Object from a Byte array", func() {
 			testInput := []byte("{\"zone\":\"us-west1-a\"}")
@@ -142,8 +135,72 @@ var _ = Describe("Bastion", func() {
 			Expect(err).To(Not(HaveOccurred()))
 			Expect(res.Zone).To(Equal(expectedMarshalOutput))
 		})
-
 	})
+
+	Describe("check Ingress Permissions", func() {
+		It("Should return a string array with ipV4 normalized addresses", func() {
+			res, err := ingressPermissions(bastion)
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(res[0]).To(Equal("213.69.151.0/24"))
+
+		})
+	})
+	Describe("check Ingress Permissions", func() {
+		It("Should return a string array with ipV4 normalized addresses", func() {
+			res, err := ingressPermissions(bastion)
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(res[0]).To(Equal("213.69.151.0/24"))
+		})
+		It("Should throw an error with invalid CIDR entry", func() {
+			bastion.Spec.Ingress = []extensionsv1alpha1.BastionIngressPolicy{
+				{IPBlock: networkingv1.IPBlock{
+					CIDR: "1234",
+				}},
+			}
+			res, err := ingressPermissions(bastion)
+			Expect(err).To(HaveOccurred())
+			Expect(res).To(BeEmpty())
+		})
+	})
+
+	Describe("check getProviderStatus", func() {
+		It("Should return an error and nil", func() {
+
+			res, err := getProviderStatus(bastion)
+			Expect(err).To(BeNil())
+			Expect(res).To(BeNil())
+		})
+		It("Should return a providerStatusRaw struct", func() {
+			bastion.Status.ProviderStatus = &runtime2.RawExtension{Raw: []byte("{\"zone\":\"us-west1-a\"}")}
+			res, err := getProviderStatus(bastion)
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(res.Zone).To(Equal("us-west1-a"))
+		})
+	})
+
+	Describe("check return values", func() {
+		It("Should return base plus disk", func() {
+			res := diskResourceName("base")
+			Expect(res).To(Equal("base-disk"))
+		})
+		It("Should return base plus nodes", func() {
+			res := nodesResourceName("base")
+			Expect(res).To(Equal("base-nodes"))
+		})
+		It("Should return base plus allow-ssh", func() {
+			res := firewallIngressAllowSSHResourceName("base")
+			Expect(res).To(Equal("base-allow-ssh"))
+		})
+		It("Should return base plus egress-worker", func() {
+			res := firewallEgressAllowOnlyResourceName("base")
+			Expect(res).To(Equal("base-egress-worker"))
+		})
+		It("Should return base plus deny-all", func() {
+			res := firewallEgressDenyAllResourceName("base")
+			Expect(res).To(Equal("base-deny-all"))
+		})
+	})
+
 })
 
 func createShootTestStruct() *gardencorev1beta1.Shoot {
@@ -157,4 +214,41 @@ func createShootTestStruct() *gardencorev1beta1.Shoot {
 				}}},
 	}
 	return shoot
+}
+
+func createGCPTestCluster() *extensions.Cluster {
+	cluster := &controller.Cluster{
+		ObjectMeta: metav1.ObjectMeta{Name: "cluster1"},
+		Shoot:      createShootTestStruct(),
+		CloudProfile: &gardencorev1beta1.CloudProfile{
+			Spec: gardencorev1beta1.CloudProfileSpec{
+				Regions: []gardencorev1beta1.Region{
+					{Name: "regionName"},
+					{Name: "us-west", Zones: []gardencorev1beta1.AvailabilityZone{
+						{Name: "us-west1-a"},
+						{Name: "us-west1-b"},
+					}},
+				},
+			},
+		},
+	}
+	return cluster
+}
+
+func createTestBastion() *extensionsv1alpha1.Bastion {
+	bastion := &extensionsv1alpha1.Bastion{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "bastionName1",
+		},
+		Spec: extensionsv1alpha1.BastionSpec{
+			DefaultSpec: extensionsv1alpha1.DefaultSpec{},
+			UserData:    nil,
+			Ingress: []extensionsv1alpha1.BastionIngressPolicy{
+				{IPBlock: networkingv1.IPBlock{
+					CIDR: "213.69.151.0/24",
+				}},
+			},
+		},
+	}
+	return bastion
 }
